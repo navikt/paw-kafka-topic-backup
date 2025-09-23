0df54c43bc31edd8986a4d3aa97e94683abf0651
Nils Martin Sande <nils.martin.sande@nav.no>
2025-09-23 14:45:25 +0200

    Fikset feilhåndtering

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use crate::app_state::AppState;
 use crate::nais_http_apis::register_nais_http_apis;
 use log::info;
 use log::error;
+use sqlx_postgres::PgPool;
 use tokio::signal::unix::{signal, SignalKind};
 use tokio::task;
 use crate::logging::init_log;
@@ -22,17 +23,46 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         is_ready: true,
         has_started: true,
     };
-    let db_config = postgres::get_database_config()?;
-    info!("Database config: {:?}", db_config);
     task::spawn(register_nais_http_apis(app_state));
     info!("HTTP server startet");
-    let pg_pool = postgres::get_pg_pool(&db_config)?;
-    info!("Postgres pool opprettet");
+    let pg_pool = match init_db() {
+        Ok(pool) => pool,
+        Err(e) => {
+            error!("Feil ved initiering av database: {}", e);
+            exit(1);
+        }
+    };
+    let _ = match await_signal().await {
+        Ok(_) => { () }
+        Err(e) => {
+            error!("Feil ved venting på signal: {}", e);
+            ()
+        }
+    };
+    pg_pool.close().await;
+    info!("Pg pool lukket");
+    Ok(())
+}
+
+async fn await_signal() -> Result<(), Box<dyn Error>> {
     let mut term_signal = signal(SignalKind::terminate())?;
     let mut interrupt_signal = signal(SignalKind::interrupt())?;
     tokio::select! {
-        _ = term_signal.recv() => {info!("\nSIGTERM mottatt, avslutter...");},
-        _ = interrupt_signal.recv() => {info!("\nSIGINT mottatt, avslutter...");},
+        _ = term_signal.recv() => {
+            info!("\nSIGTERM mottatt, avslutter...");
+            Ok(())
+        },
+        _ = interrupt_signal.recv() => {
+            info!("\nSIGINT mottatt, avslutter...");
+            Ok(())
+        },
     }
-    Ok(())
+}
+
+fn init_db() -> Result<PgPool, Box<dyn Error>> {
+    let db_config = postgres::get_database_config()?;
+    info!("Database config: {:?}", db_config);
+    let pg_pool = postgres::get_pg_pool(&db_config)?;
+    info!("Postgres pool opprettet");
+    Ok(pg_pool)
 }

4277a3672758b9809ac14cdd5c4b6a1bff122636
Nils Martin Sande <nils.martin.sande@nav.no>
2025-09-24 00:10:43 +0200

    Fikset create statement

--- src/table.rs
+++ src/table.rs
@@ -16,14 +16,14 @@ async fn _create_table(pool: &PgPool) -> Result<(), Box<dyn Error>> {
     let query = r#"
         CREATE TABLE IF NOT EXISTS data_v1 (
             id BIGSERIAL PRIMARY KEY,
-            topic VARCHAR(255) NOT NULL,
-            partition SMALLINT NOT NULL,
-            offset BIGINT NOT NULL,
+            kafka_topic VARCHAR(255) NOT NULL,
+            kafka_partition SMALLINT NOT NULL,
+            kafka_offset BIGINT NOT NULL,
             timestamp TIMESTAMP(3) WITH TIME ZONE NOT NULL,
             headers BYTEA,
             record_key BYTEA,
             record_value BYTEA,
-            UNIQUE(topic, partition, offset)
+            UNIQUE(kafka_topic, kafka_partition, kafka_offset)
         );
     "#;
     sqlx::query(query)
